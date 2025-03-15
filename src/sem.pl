0b0d0f7f681fd362045a17cbd8b1e4a950e0b419
Gideon <eignnx@gmail.com>
2025-03-15 14:49:51 -0400

    rename nop->todo, [OPERANDS]->[]

--- src/sem.pl
+++ src/sem.pl
@@ -57,7 +57,7 @@ lval(lo(RVal)) --> rval(RVal).
 lval(hi_lo(A, B)) --> rval(A), rval(B).
 lval(bitslice(LVal, Bound1 .. Bound2)) --> lval(LVal), { integer(Bound1), integer(Bound2) }.
 
-stmt_(nop) --> [].
+stmt_(todo) --> [].
 stmt_(b_push(LVal, RVal)) --> lval(LVal), rval(RVal).
 stmt_(if(Cond, Consq)) --> rval(Cond), stmt(Consq).
 stmt_(if(Cond, Consq, Alt)) --> rval(Cond), stmt(Consq), stmt(Alt).
@@ -274,459 +274,459 @@ instr_info(tnei, info{
 	title: 'Test Not Equal Immediate',
 	descr: 'Test if a register value is not equal to an immediate value.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(teqi, info{
 	title: 'Test Equal Immediate',
 	descr: 'Test if a register value is equal to an immediate value.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(addi, info{
 	title: 'Add Immediate',
 	descr: 'Add an immediate value to a register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(andi, info{
 	title: 'AND Immediate',
 	descr: 'Perform a bitwise AND between a register and an immediate value.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(ori, info{
 	title: 'OR Immediate',
 	descr: 'Perform a bitwise OR between a register and an immediate value.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 
 instr_info(xori, info{
 	title: 'XOR Immediate',
 	descr: 'Perform a bitwise XOR between a register and an immediate value.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(addicy, info{
 	title: 'Add Immediate with Carry',
 	descr: 'Add an immediate value and the carry bit to a register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(subicy, info{
 	title: 'Subtract Immediate with Carry',
 	descr: 'Sutract an immediate value and the carry bit from a register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(lsr, info{
 	title: 'Logical Shift Right',
 	descr: 'Perform a logical shift right on a register by an immediate value.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(lsl, info{
 	title: 'Logical Shift Left',
 	descr: 'Perform a logical shift left on a register by an immediate value.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(asr, info{
 	title: 'Arithmetic Shift Right',
 	descr: 'Perform an arithmetic shift right on a register by an immediate value.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 
 instr_info(add, info{
 	title: 'Add',
 	descr: 'Add the values of two registers.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(sub, info{
 	title: 'Subtract',
 	descr: 'Subtract the value of one register from another.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(and, info{
 	title: 'AND',
 	descr: 'Perform a bitwise AND between two registers.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(or, info{
 	title: 'OR',
 	descr: 'Perform a bitwise OR between two registers.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(xor, info{
 	title: 'XOR',
 	descr: 'Perform a bitwise XOR between two registers.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(mov, info{
 	title: 'Move',
 	descr: 'Move the value from one register to another.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(addcy, info{
 	title: 'Add with Carry',
 	descr: 'Add the values of two registers with carry.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(subcy, info{
 	title: 'Subtract with Carry',
 	descr: 'Subtract the value of one register from another with carry.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tl, info{
 	title: 'Test Less-than',
 	descr: 'Test if the value of one register is less than another.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tge, info{
 	title: 'Test Greater-than or Equal',
 	descr: 'Test if the value of one register is greater than or equal to another.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tb, info{
 	title: 'Test Below',
 	descr: 'Test if the value of one register is below another.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tae, info{
 	title: 'Test Above or Equal',
 	descr: 'Test if the value of one register is above or equal to another.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tne, info{
 	title: 'Test Not Equal',
 	descr: 'Test if the value of one register is not equal to another.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(teq, info{
 	title: 'Test Equal',
 	descr: 'Test if the value of one register is equal to another.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 
 instr_info(mulstep, info{
 	title: 'Multiplication Step',
 	descr: 'Computes one step in a full 16-bit by 16-bit multiplication.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 
 instr_info(pushb, info{
 	title: 'Push Byte',
 	descr: 'Push a byte from a register onto the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(pushw, info{
 	title: 'Push Word',
 	descr: 'Push a word from a register onto the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(popb, info{
 	title: 'Pop Byte',
 	descr: 'Pop a byte from the stack into a register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(popw, info{
 	title: 'Pop Word',
 	descr: 'Pop a word from the stack into a register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(callr, info{
 	title: 'Call Register',
 	descr: 'Call a subroutine at the address in a register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(jr, info{
 	title: 'Jump Register',
 	descr: 'Jump to the address in a register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(neg, info{
 	title: 'Negate',
 	descr: 'Negate the value in a register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(seb, info{
 	title: 'Sign Extend Byte',
 	descr: 'Sign extend a byte in a register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('rd.mp.lo', info{
 	title: 'Read $MP.lo',
 	descr: 'Read the low word in the system `$MP` register into a general purpose register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('rd.mp.hi', info{
 	title: 'Read $MP.hi',
 	descr: 'Read the high word in the system `$MP` register into a general purpose register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('rd.gp', info{
 	title: 'Read $GP',
 	descr: 'Read the value of the system `$GP` register into a general purpose register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('wr.gp', info{
 	title: 'Write $GP',
 	descr: 'Write a value to the system `$GP` register from a general purpose register.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 
 instr_info('NONEXE1', info{
 	title: 'Non-executable (1s Version)',
 	descr: 'Triggers a "non-executable instruction" exception. The entire instruction is 16 `1`s.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('BREAK', info{
 	title: 'Breakpoint',
 	descr: 'Trigger a breakpoint.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('HALT', info{
 	title: 'Halt',
 	descr: 'Halt the processor.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('UNIMPL', info{
 	title: 'Unimplemented',
 	descr: 'Unimplemented instruction.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(kret, info{
 	title: 'Kernel Return',
 	descr: 'Return from kernel mode.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(kcall, info{
 	title: 'Kernel Call',
 	descr: 'Call a kernel function.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(ret, info{
 	title: 'Return',
 	descr: 'Return from a subroutine.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tov, info{
 	title: 'Test Overflow',
 	descr: 'Test for overflow.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tcy, info{
 	title: 'Test Carry',
 	descr: 'Test for carry.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('clr.cy', info{
 	title: 'Clear Carry',
 	descr: 'Clear the carry flag.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('set.cy', info{
 	title: 'Set Carry',
 	descr: 'Set the carry flag.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tpush0, info{
 	title: 'Teststack Push 0',
 	descr: 'Push 0 onto the test stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tpush1, info{
 	title: 'Teststack Push 1',
 	descr: 'Push 1 onto the test stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tnot, info{
 	title: 'Teststack NOT',
 	descr: 'Perform a NOT operation on the test stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tand, info{
 	title: 'Teststack AND',
 	descr: 'Perform an AND operation on the test stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tor, info{
 	title: 'Teststack OR',
 	descr: 'Perform an OR operation on the test stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info(tdup, info{
 	title: 'Teststack Duplicate',
 	descr: 'Duplicate the top value on the test stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('prsv.mp', info{
 	title: 'Preserve $MP',
 	descr: 'Preserve the value of the `$MP` register onto the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('rstr.mp', info{
 	title: 'Restore $MP',
 	descr: 'Restore the value of the `$MP` register from the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('prsv.ts', info{
 	title: 'Preserve $TS',
 	descr: 'Preserve the value of the `$TS` register onto the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('rstr.ts', info{
 	title: 'Restore $TS',
 	descr: 'Restore the value of the `$TS` register from the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('prsv.ra', info{
 	title: 'Preserve $RA',
 	descr: 'Preserve the value of the `$RA` register onto the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('rstr.ra', info{
 	title: 'Restore $RA',
 	descr: 'Restore the value of the `$RA` register from the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('prsv.gp', info{
 	title: 'Preserve $GP',
 	descr: 'Preserve the value of the `$GP` register onto the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('rstr.gp', info{
 	title: 'Restore $GP',
 	descr: 'Restore the value of the `$GP` register from the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('prsv.cc', info{
 	title: 'Preserve $CC',
 	descr: 'Preserve the value of the `$CC` register onto the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
 instr_info('rstr.cc', info{
 	title: 'Restore $CC',
 	descr: 'Restore the value of the `$CC` register from the stack.',
 	ex: [],
-	operands: [OPERANDS],
-	sem: nop
+	operands: [],
+	sem: todo
 }).
