bf8965042e10aa6617506e7219f1b111d287530d
Gideon <eignnx@gmail.com>
2025-03-16 18:16:39 -0400

    begin work on semantics validation.

--- src/validate.pl
+++ src/validate.pl
@@ -2,6 +2,15 @@
     run_validations/0
 ]).
 
+:- use_module(library(clpfd)).
+:- op(20, fx, #).
+:- op(20, fx, $$).
+:- op(20, fx, ?).
+:- op(1050, xfx, <-).
+:- op(400, yfx, xor).
+:- op(400, yfx, and).
+:- op(400, yfx, or).
+
 :- use_module(isa).
 :- use_module(sem).
 
@@ -9,6 +18,7 @@ run_validations :-
     disprove('instr_info is not one-to-one with fmt_instr'(_)),
     disprove('ill-formed instruction semantics'(_, _)),
     disprove('undefined constant in semantic definition'(_, _)),
+    disprove('incompatible bit sizes'(_, _, _)),
     true.
 
 disprove(NegativeCheck) :-
@@ -38,4 +48,65 @@ disprove(NegativeCheck) :-
     member(constant(Constant), Emissions),
     \+ sem:def(Constant, _).
 
+'incompatible bit sizes'(instruction(Instr), val1(V1), val2(V2)) :-
+    sem:instr_info(Instr, Info),
+    isa:fmt_instr_title_description(Fmt, Instr, _, _),
+    derive:fmt_opcodebits_immbits(Fmt, _, ImmBits),
+    maplist(
+        {ImmBits}/[Op, OpName-OpTy]>>operand_immbits_name_type(Op, ImmBits, OpName, OpTy),
+        Info.operands,
+        OpNamesOpTys
+    ),
+    false.
+
+operand_immbits_name_type(reg(?Name), _, Name, i(Bits)) :- isa:register_size(Bits).
+operand_immbits_name_type(imm(?Name), ImmBits, Name, u(ImmBits)).
+operand_immbits_name_type(simm(?Name), ImmBits, Name, s(ImmBits)).
+
+supertype_subtype(i(Bits), s(Bits)).
+supertype_subtype(i(Bits), u(Bits)).
+supertype_subtype(Ty, Ty).
+% supertype_subtype(u(A), u(B)) :- #A #>= #B.
+% supertype_subtype(s(A), s(B)) :- #A #>= #B.
+
+ty(u(N), N) :- N in 1 .. 32.
+ty(s(N), N) :- N in 1 .. 32.
+ty(i(N), N) :- N in 1 .. 32.
+
+:- discontiguous inference/3.
+
+inference(_Tcx, #N, Ty) :-
+    number(N),
+    ty(Ty, Bits),
+    zcompare(Ordering, N, 0),
+    inference_bit_ord(Ordering, N, Bits, Ty).
+
+inference_bit_ord(>, N, Bits, u(Bits)) :- 2 ^ #Bits #> #N.
+inference_bit_ord(>, N, Bits, s(Bits)) :- 2 ^ (#Bits - 1) #> #N.
+inference_bit_ord(>, N, Bits, i(Bits)) :- 2 ^ #Bits #> #N.
+inference_bit_ord(<, N, Bits, s(Bits)) :- -1 * 2 ^ (#Bits - 1) #< #N.
+inference_bit_ord(<, N, Bits, i(Bits)) :- -1 * 2 ^ (#Bits - 1) #< #N.
     
+
+inference(Tcx, A + B, Ty) :-
+    inference(Tcx, A, TyA),
+    inference(Tcx, B, TyB),
+    TyA = TyB.
+
+inference(Tcx, ?Symbol, Ty) :-
+    member(Symbol-Ty, Tcx).
+
+stmt_inference(Tcx0, ?Var = Expr ; Rest0, Tcx) :-
+    inference(Tcx0, Expr, ExprTy),
+    TcxExt = [Var-ExprTy | Tcx0],
+    stmt_inference(TcxExt, Rest0, Rest, Tcx).
+
+stmt_inference(Tcx, Dst <- Src, Tcx) :-
+    inference(Tcx, Dst, DstTy),
+    inference(Tcx, Src, SrcTy),
+    DstTy = SrcTy.
+
+stmt_inference(Tcx, todo, todo, Tcx).
+
+
+
