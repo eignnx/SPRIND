ef5da6fd26101f41a33c2413219bd2d637f4ac69
Gideon <eignnx@gmail.com>
2025-04-13 15:45:35 -0400

    add `--regen` option to build script

--- build.sh
+++ build.sh
@@ -9,10 +9,7 @@ swipl --quiet --on-error=halt -f none -t generate_spec src/gen_spec.pl
 echo "Generating opcode tree graphs..."
 swipl --quiet --on-error=halt -f none -t print_dottrees src/optree2.pl
 
-# Check if any .dot files have changed since the last commit
-if git diff --quiet HEAD -- assets/graphs/*.dot; then
-    echo "No changes in .dot files. Skipping SVG generation."
-else
+generate_svgs () {
     # Process all .dot files in assets/graphs and generate .svg files in assets
     echo "Converting GraphViz files to SVGs..."
     DOT_FILES=(assets/graphs/*.dot)
@@ -25,6 +22,13 @@ else
         echo "    [$COUNT/$TOTAL_FILES] Generating $BASENAME.svg..."
         dot -Tsvg:cairo -o "assets/$BASENAME.svg" "$FILE"
     done
+}
+
+# Check if any .dot files have changed since the last commit
+if git diff --quiet HEAD -- assets/graphs/*.dot && [[ $1 != '--regen' ]]; then
+    echo "No changes in .dot files. Skipping SVG generation."
+else
+	generate_svgs
 fi
 
 echo "Done!"
